69b75e2178e6e823b81f61f9f0072d5962e109cb
quocviet1026 <quocviet1026@gmail.com>
2020-06-20 16:03:24 +0700

    rename a.js to a1.js

---
+++ a1.js
@@ -0,0 +1,9 @@
+console.log("day la file a");
+
+console.log("toi vua sua file a tren web github");
+
+console.log("toi vua sua file a tren local");
+
+<<<<<<< HEAD
+console.log("local sua de tao conflig");
+
