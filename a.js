e1a674ebe35e6b9e6810e22e7df5cb805ed7274a
quocviet1026 <quocviet1026@gmail.com>
2020-06-20 16:11:44 +0700

    creat new folder

--- a.js
+++
@@ -1,9 +0,0 @@
-console.log("day la file a");
-
-console.log("toi vua sua file a tren web github");
-
-console.log("toi vua sua file a tren local");
-
-<<<<<<< HEAD
-console.log("local sua de tao conflig");
-
